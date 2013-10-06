d75a23219fda96a9677c25537bb10064a296b7f4
Sanghee Kim <sangheestyle@gmail.com>
2013-10-06 00:24:07 -0600

    request query and save the result under query_name folder
    During writing result files, the folder name is set by the query
    
    e.g. query = "lg g2", the result path = "dumps/lg_g2"
         query = "#moto x" the result path = "_sharp_moto_x"
    
    Also, max_id.txt and last_created_at.txt are saved under the
    query_name path

--- crawlingtweets.py
+++ crawlingtweets.py
@@ -1,7 +1,7 @@
 from tweetdatastore import TweetDataStore
 import time
 
-x = TweetDataStore()
+x = TweetDataStore('lg g2')
 loop = 40
 timer = 0
 
@@ -9,7 +9,7 @@ for i in range(loop):
     if timer < 20:
         timer += 1
         print timer
-        result = x.get_search_result('lg g2', 100, x.get_max_id())
+        result = x.get_search_result(100, x.get_max_id())
         x.write_file(result)
         print x.get_max_id()
         print x.get_last_created_at()
--- tweetdatastore.py
+++ tweetdatastore.py
@@ -4,7 +4,7 @@ import json
 import os
 
 class TweetDataStore:
-    def __init__(self):
+    def __init__(self, query):
         # Obtain an OAuth2 Access Token
         self.APP_KEY = '96N3B2HWhDOh5gjc3j7PMg'
         self.APP_SECRET = '9QRbarJDJVPQzP9egDKGX5XBa9UEEaN2NKGrnONKY'
@@ -14,14 +14,21 @@ class TweetDataStore:
         self.twitter = Twython(self.APP_KEY, access_token=self.ACCESS_TOKEN)
         self.max_id = 0
         self.last_created_at = ""
+        self.query = query
         self.set_last_query_result()
 
+    def query_to_folder_name(self, query):
+        query = query.replace("#", "_sharp_")
+        query = query.replace(" ", "_")
+        return query
+
     def write_file (self, content):
-        if not os.path.exists('dumps'):
-            os.makedirs('dumps')
+        folder_path = 'dumps/' + self.query_to_folder_name(self.query)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
 
         current_path = os.path.abspath(os.curdir)
-        os.chdir('dumps')
+        os.chdir(folder_path)
 
         with open('tmp'+ str(self.max_id) + '.txt', 'w') as handle:
             json.dump(content, handle)
@@ -29,20 +36,23 @@ class TweetDataStore:
         os.chdir(current_path)
         handle.close()
 
-    def get_search_result (self, query, num_tweet , last_id = 0):
-        result = self.twitter.search(q = query, count = num_tweet, max_id = last_id)
+    def get_search_result (self, num_tweet , last_id = 0):
+        result = self.twitter.search(q = self.query, count = num_tweet, max_id = last_id)
         for item in result['statuses']:
             self.set_last_max_id(item['id'])
             self.set_last_created_at(item['created_at'])
         return result
 
     def set_last_query_result (self):
-        if os.path.exists('max_id.txt') and os.path.exists('created_at.txt'):
-            with open('max_id.txt', 'r') as handle:
+        max_id_path = "dumps/" + self.query_to_folder_name(self.query) + "/max_id.txt"
+        created_at_path = "dumps/" + self.query_to_folder_name(self.query) + "/created_at.txt"
+
+        if os.path.exists(max_id_path) and os.path.exists(created_at_path):
+            with open(max_id_path, 'r') as handle:
                 self.set_last_max_id(int(handle.readline()))
                 print self.get_max_id()
             handle.close()
-            with open('created_at.txt', 'r') as handle:
+            with open(created_at_path, 'r') as handle:
                 self.set_last_created_at(handle.readline())
                 print self.last_created_at
             handle.close()
@@ -50,18 +60,30 @@ class TweetDataStore:
             self.set_last_max_id(0)
 
     def set_last_max_id (self, max_id):
-        with open('max_id.txt', 'w') as handle:
+        max_id_path = "dumps/" + self.query_to_folder_name(self.query)
+        if not os.path.exists(max_id_path):
+            os.makedirs(max_id_path)
+        current_path = os.path.abspath(os.curdir)
+        os.chdir(max_id_path)
+        with open("max_id.txt", 'w') as handle:
             handle.write(str(max_id))
         handle.close()
+        os.chdir(current_path)
         self.max_id = max_id
 
     def get_max_id (self):
         return self.max_id
 
     def set_last_created_at (self, created_at):
-        with open('created_at.txt', 'w') as handle:
+        created_at_path = "dumps/" + self.query_to_folder_name(self.query)
+        if not os.path.exists(created_at_path):
+            os.makedirs(created_at_path)
+        current_path = os.path.abspath(os.curdir)
+        os.chdir(created_at_path)
+        with open("created_at.txt", 'w') as handle:
             handle.write(created_at)
         handle.close()
+        os.chdir(current_path)
         self.last_created_at = created_at
 
     def get_last_created_at (self):
