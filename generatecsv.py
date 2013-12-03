c1e7b21944e1f02b8d60cad555ef504b6adb1124
Sanghee Kim <sangheestyle@gmail.com>
2013-12-02 23:00:20 -0700

    mod: remove index during making csv file

--- generatecsv.py
+++ generatecsv.py
@@ -17,10 +17,10 @@ tweet_fields = ['created_at',
 x = TweetDataAnalyzer()
 
 moto_x = x.load_jsons_to_dataframe('dumps/moto_x', tweet_fields)
-moto_x.to_csv('moto_x_raw.csv', sep='\t', encoding='utf-8')
+moto_x.to_csv('moto_x_raw.csv', index=False, sep='\t', encoding='utf-8')
 
 lg_g2 = x.load_jsons_to_dataframe('dumps/lg_g2', tweet_fields)
-lg_g2.to_csv('lg_g2_raw.csv', sep='\t', encoding='utf-8')
+lg_g2.to_csv('lg_g2_raw.csv', index=False, sep='\t', encoding='utf-8')
 
 nexus_5 = x.load_jsons_to_dataframe('dumps/nexus_5', tweet_fields)
-nexus_5.to_csv('nexus_5_raw.csv', sep='\t', encoding='utf-8')
+nexus_5.to_csv('nexus_5_raw.csv', index=False, sep='\t', encoding='utf-8')
