9d950318248311833e6da8a1df551eb6b83b9cf0
Sanghee Kim <sangheestyle@gmail.com>
2013-12-07 00:02:51 -0700

    add: creating dataframe from csv generated by tweetdataframe only
    Informative:
    In IPython, you can access the instance of TweetDataframe Class with following
    
    %run tweetdataframe.py
    %dfs['moto_x].df
    
    Also, you may need to check the path, SAMPLE_CSV_PATH in tweetdataframe.py

--- tweetdataframe.py
+++ tweetdataframe.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 class TweetDataframe:
 
-    def __init__(self, root, tweet_fields=None, user_tweet_fields=None):
+    def __init__(self, path, tweet_fields=None, user_tweet_fields=None):
         if tweet_fields == None:
             tweet_fields = ['created_at',
                              'id',
@@ -22,13 +22,19 @@ class TweetDataframe:
                                  'followers_count',
                                  'friends_count'
                                 ]
-        self.root = root
+        if os.path.isfile(path) and path.endswith('.csv'):
+            input_type = 'csv'
+        else:
+            input_type = None
+
+        self.path = path
+        self.input_type = input_type
         self.tweet_fields = tweet_fields
         self.user_tweet_fields = user_tweet_fields
         self.df = pd.DataFrame()
         self.set_formatted_dataframe()
 
-    def from_json_file(self, file_path):
+    def from_json(self, file_path):
         try:
             file = open(file_path)
             contents = json.load(file)
@@ -38,14 +44,27 @@ class TweetDataframe:
         except Exception, e:
             print "Failed to make DataFrame: ", e
 
-    def from_json_files(self):
-        for root, dir_names, file_names in os.walk(self.root):
-            for file_name in file_names:
-                if not file_name.endswith('.txt'):
-                    continue
-                else:
+    def from_csv(self, file_path):
+        try:
+            file = open(file_path)
+            return pd.DataFrame.from_csv(file_path, 0, '\t')
+        except Exception, e:
+            print "Failed to make DataFrame: ", e
+
+    def from_files(self):
+        if os.path.isfile(self.path):
+            if self.path.endswith('.txt'):
+                self.df = self.df.append(self.from_json(self.path))
+            if self.path.endswith('.csv'):
+                self.df = self.df.append(self.from_csv(self.path))
+        elif os.path.isdir(self.path):
+            for root, dir_names, file_names in os.walk(self.path):
+                for file_name in file_names:
                     path = os.path.join(root, file_name)
-                    self.df = self.df.append(self.from_json_file(path))
+                    if file_name.endswith('.txt'):
+                        self.df = self.df.append(self.from_json(path))
+                    if file_name.endswith('.csv'):
+                        self.df = self.df.append(self.from_csv(path))
 
     def get_user_field(self, users, field):
         new_column = []
@@ -62,14 +81,17 @@ class TweetDataframe:
         del self.df['user']
 
     def set_formatted_dataframe(self):
-        self.from_json_files()
-        self.move_user_fields_to_columns()
+        self.from_files()
+        if self.input_type == None:
+            self.move_user_fields_to_columns()
 
     def to_csv(self, file_name, index=False, sep='\t', encoding='utf=8'):
         self.df.to_csv(file_name, index=index, sep=sep, encoding=encoding)
 
 if __name__ == "__main__":
     from datetime import datetime
+    DUMPS_PATH = "dumps/"
+    SAMPLE_CSV_PATH = "sample/csv/"
 
     device_names = ['moto_x',
                     'lg_g2',
@@ -78,11 +100,11 @@ if __name__ == "__main__":
 
     def generate_csv(device_name):
         print "=== BEGIN: " + device_name + " " + str(datetime.now())
-        x = TweetDataframe("dumps/" + device_name)
+        x = TweetDataframe(DUMPS_PATH + device_name)
         print x.df
         print "=== Procesing CSV" + " " + str(datetime.now())
         x.to_csv(device_name + '.csv')
         print "=== END: " + device_name + " " + str(datetime.now())
 
-    for device_name in device_names:
-        generate_csv(device_name)
+    dfs = {device_name: TweetDataframe(SAMPLE_CSV_PATH + device_name + ".csv")
+           for device_name in device_names}
