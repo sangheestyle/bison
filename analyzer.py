feeebe0ffbdfc97ca9ea60d2c5d6c9a18c755673
Sanghee Kim <sangheestyle@gmail.com>
2013-12-08 15:37:34 -0700

    refactoring top_n_source method due to it's length

--- analyzer.py
+++ analyzer.py
@@ -24,20 +24,28 @@ class Analyzer(TweetDataframe):
     def top_n_source(self, n):
         df_source = self.df.source
         source_counts = df_source.value_counts()[:n]
+        n_source = self.text_only_source(source_counts)
+        return pd.DataFrame(n_source, columns=['source', 'count'])
+
+    def text_only_source(self, series):
         refined_source = []
         counts = []
         n_source = {}
-        for source, count in source_counts.iteritems():
-            result = re.search('>(.*)</a>', source)
-            if result == None:
-                result = source
-            else:
-                result = result.group(1)
+        for source, count in series.iteritems():
+            result = self.text_from_href(source)
             refined_source.append(result)
             counts.append(count)
         n_source['source'] = refined_source
         n_source['count'] = counts
-        return pd.DataFrame(n_source, columns=['source', 'count'])
+        return n_source
+
+    def text_from_href(self, source):
+        result = re.search('>(.*)</a>', source)
+        if result == None:
+            result = source
+        else:
+            result = result.group(1)
+        return result
 
 if __name__ == "__main__":
     SAMPLE_CSV_PATH = "sample/csv/moto_x.csv"
