22cf41b7c0d326e949d7b3d8a3fb44b3c8d15354
Sanghee Kim <sangheestyle@gmail.com>
2013-12-08 15:11:07 -0700

    get dataframe of top N source
    This implementation is about getting a dataframe of top N source.
    For me, this implementation is little bit confused due to various type of
    source such as twitterfeed, dlvr.it, and IFTTT. What are those?
    Originally, I just want to know what kind of device do people who was
    twitting this tweet about devices use.
    I think I need to know what those are such as dlvr.it and how can treat
    those kind of things? Is it based on which device?

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,5 @@
+import re
+import pandas as pd
 from tweetdataframe import TweetDataframe
 
 class Analyzer(TweetDataframe):
@@ -20,9 +22,26 @@ class Analyzer(TweetDataframe):
         pass
 
     def top_n_source(self, n):
-        pass
+        df_source = self.df.source
+        source_counts = df_source.value_counts()[:n]
+        refined_source = []
+        counts = []
+        n_source = {}
+        for source, count in source_counts.iteritems():
+            result = re.search('>(.*)</a>', source)
+            if result == None:
+                result = source
+            else:
+                result = result.group(1)
+            refined_source.append(result)
+            counts.append(count)
+        n_source['source'] = refined_source
+        n_source['count'] = counts
+        return pd.DataFrame(n_source, columns=['source', 'count'])
 
 if __name__ == "__main__":
     SAMPLE_CSV_PATH = "sample/csv/moto_x.csv"
     x = Analyzer(SAMPLE_CSV_PATH)
-    print x.df
+    #print x.df
+    x_tns = x.top_n_source(10)
+    print x_tns
